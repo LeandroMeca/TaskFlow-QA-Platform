a6191bac9a4bd9e13d9483b74ca3151fe8f0f52f
LeandroMeca <leandromeca20@outlook.com>
2026-03-30 04:30:55 -0300

    created file postman to ci

--- backend/src/main/java/com/task/manager/controller/TaskController.java
+++ backend/src/main/java/com/task/manager/controller/TaskController.java
@@ -2,7 +2,6 @@ package com.task.manager.controller;
 
 import com.task.manager.entity.Task;
 import com.task.manager.service.TaskService;
-import org.hibernate.engine.spi.EntityEntry;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -17,24 +16,23 @@ public class TaskController {
     private TaskService taskService;
 
     @GetMapping
-    public List<Task> list(){
+    public List<Task> list() {
         return taskService.findAll();
     }
 
     @PostMapping
-    public Task create(@RequestBody Task task){
+    public Task create(@RequestBody Task task) {
         return taskService.save(task);
     }
 
     @PutMapping("/{id}")
-    public ResponseEntity<Task> updateTask(@PathVariable Long id, @RequestBody Task task){
-        Task updateTask = taskService.updateTask(id,task);
+    public ResponseEntity<Task> updateTask(@PathVariable Long id, @RequestBody Task task) {
+        Task updateTask = taskService.updateTask(id, task);
         return ResponseEntity.ok(updateTask);
     }
 
-
     @DeleteMapping("/{id}")
-    public ResponseEntity<Void> deleteTask(@PathVariable Long id){
+    public ResponseEntity<Void> deleteTask(@PathVariable Long id) {
         taskService.deletetask(id);
         return ResponseEntity.noContent().build();
     }
